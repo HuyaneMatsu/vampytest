62f1edeb721772513bbed416bd1e23c1536ba307
HuyaneMatsu <re.ism.tm@gmail.com>
2022-02-24 15:00:45 +0100

    imrpove resolution

--- vampytest/collect_test_files.py
+++ vampytest/collect_test_files.py
@@ -31,10 +31,60 @@ def iter_collect_test_files(path):
         yield TestFile(path)
     
     if is_directory(path):
-        yield from iter_tests_from_directory(path)
+        yield from iter_tests_from_directory(path, False)
 
 
-def iter_tests_from_directory(directory_path):
+def is_test_file_name(file_name):
+    """
+    Returns whether the given file name is the name of a test file.
+    
+    Parameters
+    ----------
+    file_name : `str`
+        A file's name.
+    
+    Returns
+    -------
+    is_test_file_name : `bool`
+    """
+    if file_name == 'test.py':
+        return True
+    
+    if file_name.startswith('test_') and file_name.endswith('.py'):
+        return True
+    
+    if file_name.endswith('_tests.py'):
+        return True
+    
+    return False
+
+
+def is_test_directory_name(directory_name):
+    """
+    Returns whether the given directory name is a name of a test directory.
+    
+    Parameters
+    ----------
+    directory_name : `str`
+        A directory's name.
+    
+    Returns
+    -------
+    is_test_directory_name : `bool`
+    """
+    if directory_name == 'tests':
+        return True
+    
+    if directory_name.startswith('test_'):
+        return True
+    
+    if directory_name.endswith('_tests'):
+        return True
+    
+    return False
+
+
+def iter_tests_from_directory(directory_path, within_test_directory):
     """
     Iterates over a directory discovering test files.
     
@@ -44,6 +94,8 @@ def iter_tests_from_directory(directory_path):
     ----------
     directory_path : `str`
         Path to the directory.
+    within_test_directory : `bool`
+        Defines whether
     
     Yields
     ------
@@ -53,14 +105,8 @@ def iter_tests_from_directory(directory_path):
         file_path = join_paths(directory_path, file_name)
         
         if is_file(file_path):
-            if (
-                file_name == 'test.py' or
-                (
-                    file_name.startswith('test_') and
-                    file_name.endswith('.py')
-                )
-            ):
+            if within_test_directory and is_test_file_name(file_name):
                 yield TestFile(file_path)
         
         if is_directory(file_path):
-            yield from iter_tests_from_directory(file_path)
+            yield from iter_tests_from_directory(file_path, is_test_directory_name(file_name))
