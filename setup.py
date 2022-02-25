4213336defc12489af4e9a5d9bc8401aceb0214b
HuyaneMatsu <re.ism.tm@gmail.com>
2022-02-25 10:12:08 +0100

    add impl to setup

--- setup.py
+++ setup.py
@@ -34,7 +34,10 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         #'Programming Language :: Python :: 3.11',
-
+        
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
